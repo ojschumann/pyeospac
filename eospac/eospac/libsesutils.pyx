10188a190f64f7cae18aa085758ea49ae1668219
Roman Yurchak <rth@crans.org>
2016-04-15 19:12:15 +0200

    Added version number

---
+++ eospac/eospac/libsesutils.pyx
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import numpy as np
+cimport numpy as np
+
+import constants as cst
+from libc.stdlib cimport malloc, free
+# to use the Numpy-C-API from Cython
+np.import_array()
+
+
+ctypedef np.int32_t int32
+ctypedef np.float64_t float64
+
+
+cdef extern from "eos_types_internal.h":
+    struct eos_Option:
+       pass
+
+cdef extern from "":
+    struct eos_Data:
+       pass
+
+cdef extern from "eos_SesUtils.h":
+    cdef int  eos_SesCleanFileCache()
+
+
+
+cpdef eospac_clean_cache():
+    return eos_SesCleanFileCache()
+
